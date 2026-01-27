36aa2056ea807dd7aa1c629f57ff77d5e3ac1b7f
PS164435 <100274100+PS164435@users.noreply.github.com>
2026-01-27 15:52:56 +0100

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,90 @@
+import React, {useCallback, useEffect, useState} from "react";
+import {Routes, Route, Link} from "react-router-dom";
+import Tables from "./Tables";
+import Calculator from "./Calculator";
+import './main.css';
+import Login from "./Login";
+import Register from "./Register";
+import API_URL from "./api";
+
+function App() {
+
+    const [loggedIn, setLoggedIn] = useState(!!localStorage.getItem("token"));
+    const [userName, setUserName] = useState(null);
+
+    const logout = () => {
+        localStorage.removeItem("token");
+        setLoggedIn(false)
+        setUserName(null);
+    };
+
+    const myName = useCallback(async () => {
+        const token = localStorage.getItem("token");
+        if (!token) return;
+        try {
+            const res = await fetch(`${API_URL}/accounts/name/`, {
+                headers: {
+                    Authorization: `Token ${token}`,
+                },
+            });
+            if (!res.ok) {
+                logout();
+                return;
+            }
+            const data = await res.json();
+            setUserName(data.first_name);
+        } catch (err) {
+            alert("Błąd pobierania nazwy")
+        }
+    }, [API_URL, logout]);
+
+    useEffect(() => {
+        if (loggedIn) {myName();}
+    }, [loggedIn, myName]);
+
+
+  return (
+    <div>
+      <nav>
+        <Link to="/tables">Baza Danych</Link>
+        <Link to="/calculator">Kalkulator</Link>
+
+            <div className="nav-auth">
+              {loggedIn ? (
+                  <>
+                  <span className="nav-status">{userName ? `${userName}` : "Zalogowano"}</span>
+                  <button className="button-logout" onClick={logout}>Wyloguj</button>
+                  </>
+              ) : (
+                  <>
+                      <Link to="/login">Sign in</Link>
+                      <Link to="/register">Sign up</Link>
+                  </>
+              )}
+          </div>
+
+      </nav>
+      <Routes>
+        <Route path="/tables" element={<Tables/>}/>
+        <Route path="/calculator" element={<Calculator/>}/>
+
+        {!loggedIn && (
+              <>
+              <Route
+                path="/login"
+                element={<Login onAuth={() => {setLoggedIn(true); myName();}} />}
+              />
+              <Route
+                path="/register"
+                element={<Register onAuth={() => {setLoggedIn(true); myName()}} />}
+              />
+              </>
+        )}
+
+        <Route path="*" element={<Tables />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Login.js
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import API_URL from "./api";
+
+function Login({ onAuth }) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+    const handleLogin = async (e) => {
+        e.preventDefault()
+        setError(null);
+
+        try {
+            const res = await fetch(`${API_URL}/accounts/login/`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password,
+                }),
+            });
+
+            let data;
+            try {
+                data = await res.json();
+            } catch {
+                setError("Nieprawidłowa odpowiedź serwera");
+                return;
+            }
+            console.log("LOGIN RESPONSE:", res.status, data);
+
+            if (!res.ok) {
+                setError(data.error || "Błąd logowania");
+                return;
+            }
+
+            localStorage.setItem("token", data.token);
+            onAuth();
+
+        } catch (err) {
+            setError("Błąd połączenia z serwerem")
+        }
+    };
+
+    return (
+        <form onSubmit={handleLogin}>
+            <h2>Login</h2>
+            <input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+            <button type="submit">Login</button>
+            {error && <p style={{ color: "red" }}>{error}</p>}
+        </form>
+    );
+
+}
+export default Login;
