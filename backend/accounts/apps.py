3cf5e645003d245f40fa90cadb11c3574037ded3
PS164435 <100274100+PS164435@users.noreply.github.com>
2026-01-28 13:07:57 +0100

    Refactor user model retrieval in AccountsConfig

--- backend/accounts/apps.py
+++ backend/accounts/apps.py
@@ -1,12 +1,13 @@
 from django.apps import AppConfig
 from django.conf import settings
-from django.contrib.auth.models import User
+from django.contrib.auth.models import get_user_model
 
 class AccountsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = 'accounts'
 
     def ready(self):
+        User = get_user_model()
         username = settings.ADMIN_USERNAME
         email = settings.ADMIN_EMAIL
         password = settings.ADMIN_PASSWORD
@@ -20,4 +21,5 @@ class AccountsConfig(AppConfig):
                 email=email,
                 password=password
             )
+
 
