316dafcc2c78989824f220286f0e4c2bb8ef0ce8
PS164435 <100274100+PS164435@users.noreply.github.com>
2026-01-28 12:59:47 +0100

    Update apps.py

--- backend/accounts/apps.py
+++ backend/accounts/apps.py
@@ -1,5 +1,23 @@
 from django.apps import AppConfig
-
+from django.conf import settings
+from django.contrib.auth.models import User
 
 class AccountsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
     name = 'accounts'
+
+    def ready(self):
+        username = settings.ADMIN_USERNAME
+        email = settings.ADMIN_EMAIL
+        password = settings.ADMIN_PASSWORD
+    
+        if not username or not password:
+            return
+        
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password
+            )
+
