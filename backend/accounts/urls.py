2f0f718e0933068eed14b89a6d6c63b39262560c
PS164435 <100274100+PS164435@users.noreply.github.com>
2026-01-29 17:07:27 +0100

    Update urls.py

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RegisterViewSet, SavedCalculatorViewSet, NameView
+from .views import RegisterViewSet, LoginViewSet, SavedCalculatorViewSet, NameView
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 
 router = DefaultRouter()
@@ -8,10 +8,11 @@ router.register('calculators', SavedCalculatorViewSet, basename='calculator')
 
 urlpatterns = [
     path('register/', RegisterViewSet.as_view(), name='register'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', LoginViewSet.as_view(), name='token'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('name/', NameView.as_view(), name='name'),
     path('', include(router.urls)),
 ]
+
 
 urlpatterns += router.urls
