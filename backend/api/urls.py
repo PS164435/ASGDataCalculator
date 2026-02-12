acca61ab2a036a7f386dcc8b18912fbe5ad66550
PS164435 <100274100+PS164435@users.noreply.github.com>
2026-02-12 15:29:41 +0100

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,12 +1,14 @@
 from rest_framework.routers import DefaultRouter
-from .views import ReplicaViewSet, AttachmentViewSet, AmmunitionViewSet
+from .views import ReplicaViewSet, AttachmentViewSet, AmmunitionViewSet, UserViewSet
 from django.urls import path,include
 
 router = DefaultRouter()
 router.register('replicas', ReplicaViewSet, basename='replica')
 router.register('attachments', AttachmentViewSet, basename='attachment')
 router.register('ammunition', AmmunitionViewSet, basename='ammunition')
+router.register('users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('',include(router.urls)),
+
 ]
