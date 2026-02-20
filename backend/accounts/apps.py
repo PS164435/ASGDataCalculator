b6d644ff5e8808a8fc753a0431098c120cbe1e16
PS164435 <100274100+PS164435@users.noreply.github.com>
2026-02-20 15:19:24 +0100

    Update apps.py

--- backend/accounts/apps.py
+++ backend/accounts/apps.py
@@ -6,23 +6,8 @@ class AccountsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = 'accounts'
 
-    def ready(self):
-        User = get_user_model()
-        username = settings.ADMIN_USERNAME
-        email = settings.ADMIN_EMAIL
-        password = settings.ADMIN_PASSWORD
-        first_name = getattr(settings, "ADMIN_FIRST_NAME", "")
     
-        if not username or not password:
-            return
-        
-        if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(
-                username=username,
-                email=email,
-                password=password,
-                first_name=first_name,
-            )
+
 
 
 
