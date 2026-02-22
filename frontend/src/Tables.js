8d607ae41a7013ef902e0e3b58522f6a08ccc586
PS164435 <100274100+PS164435@users.noreply.github.com>
2026-02-22 16:56:16 +0100

    Update Tables.js

--- frontend/src/Tables.js
+++ frontend/src/Tables.js
@@ -8,6 +8,10 @@ function Tables() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
+    const [selectedCity, setSelectedCity] = useState("Warszawa");
+    const [weather, setWeather] = useState(null);
+    const [loading2, setLoading2] = useState(true);
+
     const fetchData = async () => {
         try {
             const [replicasRes, attachmentsRes, ammunitionRes] = await Promise.all([
@@ -46,13 +50,40 @@ function Tables() {
 
     const cities = {
         Warszawa: { lat:52.23, lon: 21.01 },
-        Kraków: { lat:20.06, lon: 19.94 },
+        Kraków: { lat:50.06, lon: 19.94 },
         Gdańsk: { lat:54.35, lon: 18.65 },
         Wrocław: { lat:51.11, lon: 17.03 },
         Poznań: { lat:52.40, lon: 16.93 },
     };
 
+    
+
+    const fetchWeather = async (cityName) => {
+        const {lat,lon} = cities[cityName];
+        setLoading2(true);
+        try {
+            const res = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,windspeed_10m_max,winddirection_10m_dominant&timezone=Europe/Warsaw`);
+            if (!res.ok) throw new Error("Błąd pobierania danych pogody");
+            const data = await res.json();
+            setWeather(data.daily);
+        } catch (err) {
+            console.error(err);
+        }
+        setLoading2(false);
+    };
+
+    useEffect(() => {
+        fetchWeather(selectedCity);
+    }, [selectedCity]);
+
+    const getWindArrow = (degree) => { 
+        const directions = ["⬆️", "↗️", "➡️", "↘️", "⬇️", "↙️", "⬅️", "↖️"];
+        const index = Math.round(degree/45)%8;
+        return directions[index];
+    };
+                
     return (
+        <>
         <div>
             <h1>ASG Database</h1>
             <h2>Repliki</h2>
@@ -143,11 +174,47 @@ function Tables() {
                 </table>
             )}
         </div>
+
+        <div>
+            <h2>Pogoda 7dni</h2>
+
+            <select value={selectedCity} onChange={(e) => setSelectedCity(e.target.value)}>
+                {Object.keys(cities).map((city) => (<option key={city} value={city}>{city}</option>))}
+            </select>
+                {loading2 && <p>Loading...</p>}
+                {weather && (
+                    <table border="1" cellPadding="10" style={{marginTop: "1rem" }}>
+                        <thead>
+                            <tr>
+                                <th>Data</th>
+                                <th>Temp Min</th>
+                                <th>Temo Max</th>
+                                <th>Opady (mm)</th>
+                                <th>Wiatr (km/h)</th>
+                                <th>Kierunek</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {weather.time.map((date,index) => (
+                                <tr key={date}>
+                                    <td>{date}</td>
+                                    <td>{weather.temperature_2m_min[index]}</td>
+                                    <td>{weather.temperature_2m_max[index]}</td>
+                                    <td>{weather.precipitation_sum[index]}</td>
+                                    <td>{weather.windspeed_10m_max[index]}</td>
+                                    <td>{getWindArrow(weather.winddirection_10m_dominant[index])}{" "}({weather.winddirection_10m_dominant[index]})</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+        </div>
+        </>
     );
 }
 
 
 export default Tables;
+
 
 
 
