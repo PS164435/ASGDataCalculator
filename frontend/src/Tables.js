30f956f9ebf66e96c915fcad5d974f91465f4948
PS164435 <100274100+PS164435@users.noreply.github.com>
2026-02-23 12:11:54 +0100

    Update Tables.js

--- frontend/src/Tables.js
+++ frontend/src/Tables.js
@@ -2,44 +2,10 @@ import React, {useEffect, useState} from "react";
 import API_URL from "./api";
 
 function Tables() {
-    const [replicas, setReplicas] = useState([]);
-    const [attachments, setAttachments] = useState([]);
-    const [ammunition, setAmmunition] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-
     const [selectedCity, setSelectedCity] = useState("Warszawa");
     const [weather, setWeather] = useState(null);
-    const [loading2, setLoading2] = useState(true);
-
-    const fetchData = async () => {
-        try {
-            const [replicasRes, attachmentsRes, ammunitionRes] = await Promise.all([
-                fetch(`${API_URL}/api/replicas/`),
-                fetch(`${API_URL}/api/attachments/`),
-                fetch(`${API_URL}/api/ammunition/`),
-            ]);
-
-            if (!replicasRes.ok || !attachmentsRes.ok || !ammunitionRes.ok) {
-                throw new Error("API connection Error");
-            }
-
-            const [replicasData, attachmentsData, ammunitionData] = await Promise.all([
-                replicasRes.json(),
-                attachmentsRes.json(),
-                ammunitionRes.json(),
-            ]);
-
-            setReplicas(replicasData);
-            setAttachments(attachmentsData);
-            setAmmunition(ammunitionData);
-            setLoading(false);
-        } catch (err) {
-            console.error("Error:", err);
-            setError(err.message);
-            setLoading(false);
-        }
-    };
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     const cities = {
         Warszawa: { lat:52.23, lon: 21.01 },
@@ -81,98 +47,6 @@ function Tables() {
     };
                 
     return (
-        <>
-        <div>
-            <h1>ASG Database</h1>
-            <h2>Repliki</h2>
-            {replicas.length === 0 ? (
-                <p>Brak danych.</p>
-            ) : (
-                <table border="1" cellPadding="10" style={{borderCollapse: "collapse", marginBottom: "2rem"}}>
-                    <thead>
-                    <tr>
-                        <th>Nazwa</th>
-                        <th>Kategoria</th>
-                        <th>Rozmiar</th>
-                        <th>Waga</th>
-                        <th>Opis</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {replicas.map((r) => (
-                        <tr key={r.id}>
-                            <td>{r.name}</td>
-                            <td>{r.category}</td>
-                            <td>{r.size}</td>
-                            <td>{r.weight}</td>
-                            <td>{r.description}</td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-            )}
-
-            <h2>Dodatki</h2>
-            {attachments.length === 0 ? (
-                <p>Brak danych.</p>
-            ) : (
-                <table border="1" cellPadding="10" style={{borderCollapse: "collapse", marginBottom: "2rem"}}>
-                    <thead>
-                    <tr>
-                        <th>Nazwa</th>
-                        <th>Firma</th>
-                        <th>Kategoria</th>
-                        <th>Rozmiar</th>
-                        <th>Waga</th>
-                        <th>Opis</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {attachments.map((r) => (
-                        <tr key={r.id}>
-                            <td>{r.name}</td>
-                            <td>{r.company}</td>
-                            <td>{r.category}</td>
-                            <td>{r.size}</td>
-                            <td>{r.weight}</td>
-                            <td>{r.description}</td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-            )}
-
-            <h2>Amunicja</h2>
-            {ammunition.length === 0 ? (
-                <p>Brak danych.</p>
-            ) : (
-                <table border="1" cellPadding="10" style={{borderCollapse: "collapse", marginBottom: "2rem"}}>
-                    <thead>
-                    <tr>
-                        <th>Nazwa</th>
-                        <th>Firma</th>
-                        <th>Waga</th>
-                        <th>Ilość</th>
-                        <th>Biodegradowalne</th>
-                        <th>Świecąca</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {ammunition.map((r) => (
-                        <tr key={r.id}>
-                            <td>{r.name}</td>
-                            <td>{r.company}</td>
-                            <td>{r.weight}</td>
-                            <td>{r.amount}</td>
-                            <td>{r.biodegradable ? "YES" : "NO"}</td>
-                            <td>{r.glowing ? "YES" : "NO"}</td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-            )}
-        </div>
-
         <div>
             <h2>Pogoda 7dni</h2>
 
@@ -207,12 +81,12 @@ function Tables() {
                     </table>
                 )}
         </div>
-        </>
     );
 }
 
 
 export default Tables;
+
 
 
 
