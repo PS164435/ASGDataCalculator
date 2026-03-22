cacc02c26912381f8a4763ff0a0cc58bfefdbf22
PS164435 <100274100+PS164435@users.noreply.github.com>
2026-03-22 18:02:55 +0100

    Update Refresh.js

--- frontend/src/Refresh.js
+++ frontend/src/Refresh.js
@@ -3,37 +3,30 @@ import API_URL from "./api";
 export async function fetchAuthRefr(url, options = {}) {
     const access = localStorage.getItem("access");
 
-    options.headers = {
-        ...(options.headers || {}),
-        Authorization: `Bearer ${access}`,
-    };
+    options.headers = { ...(options.headers || {}), Authorization: `Bearer ${access}`, };
 
-    let res = await fetch(url, options);
+    const res = await fetch(url, options);
 
-    if (res.status === 401) {
+    if (res.status === 401) { 
         const refresh = localStorage.getItem("refresh");
-
         const refreshRes = await fetch(`${API_URL}/accounts/token/refresh/`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ refresh }),
+            body: JSON.stringify({refresh}),
         });
 
         if (!refreshRes.ok) {
             localStorage.removeItem("access");
             localStorage.removeItem("refresh");
-            window.location.href = "/login";
             return;
         }
 
         const data = await refreshRes.json();
         localStorage.setItem("access", data.access);
-
         options.headers.Authorization = `Bearer ${data.access}`;
         res = await fetch(url, options);
     }
-
     return res;
 }
