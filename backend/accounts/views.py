f80ba389c33c9341a7b6cc241b0111f0afc500b3
PS164435 <100274100+PS164435@users.noreply.github.com>
2026-03-23 14:25:17 +0100

    Update views.py

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,10 +1,8 @@
 from rest_framework import status
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAdminUser
-from rest_framework import viewsets
 from .models import SavedCalculator, UserCounter
 from .serializers import RegisterSerializer, UserSavedCalculatorsSerializer, LoginSerializer, AdminSavedCalculatorsSerializer, AdminUsersSerializer
 from django.contrib.auth.models import User
